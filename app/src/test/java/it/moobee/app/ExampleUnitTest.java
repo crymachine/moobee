b4a10d8cb1392d5e8fa950f5429aa757125c6163
Daniele Maria De Rossi <daniele.derossi.phone@gmail.com>
2016-12-07 17:54:48 +0100

    Add files via upload

---
+++ app/src/test/java/it/moobee/app/ExampleUnitTest.java
@@ -0,0 +1,21 @@
+package it.moobee.app;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        
+    }
+}
